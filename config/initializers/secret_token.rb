9813288fa673605a581c6611326e45caec684925
liyijie <liyijie825@gmail.com>
2013-09-02 09:58:51 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TypingCompetition::Application.config.secret_token = '444ad36e485f80ae5a27774e77d2b526c29df7cd67dd5f57ffa37fdf5cc2cd16119869c9834ee2263696524b18a2c17bc8721624a1ec23a2b72592eaeb4b6168'
