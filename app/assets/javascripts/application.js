a149615ca27411a5477acf7e8f028a62922cafe3
liyijie <liyijie825@gmail.com>
2013-09-02 10:02:44 +0800

    init ember.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,13 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require typing_competition
+
+// for more details see: http://emberjs.com/guides/application/
+TypingCompetition = Ember.Application.create();
+
 //= require_tree .
---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+TypingCompetition.Router.map ()->
+  # @resource('posts')
+
